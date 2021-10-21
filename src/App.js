2e6e4ca670c5a574bb8e46802d3319e54883fe04
denish kunadiya <denish.login@gmail.com>
2021-10-21 11:37:42 +0530

    shop page is created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter,Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
 
- const App = () => {
-   
+const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </BrowserRouter>
   );
-}
+};
 export default App;
