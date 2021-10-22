5f033e53cda498d76e8ae96e722d7281e6dfd1d5
denish kunadiya <denish.login@gmail.com>
2021-10-22 18:27:39 +0530

    build signup page and to be continued

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component";
-
+import SignInAnSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CustomButton from "./components/custom-button/custom-button.component";
 const App = () => {
   return (
     <BrowserRouter>
@@ -13,8 +14,10 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAnSignUpPage} />
       </Switch>
       </BrowserRouter>
   );
 };
 export default App;
+ 
