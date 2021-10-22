59f92e0db738f6c7eb430bcbf12d7406c2a43faa
denish kunadiya <denish.login@gmail.com>
2021-10-22 17:17:23 +0530

    here we created our header component and set it with button with link and crown logo of header

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
-
+import Header from "./components/header/header.component";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
       </Switch>
-    </BrowserRouter>
+      </BrowserRouter>
   );
 };
 export default App;
